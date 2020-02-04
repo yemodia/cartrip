858b3d526c66cd04f705ca89368f15f931d806d4
yerimdia <yerimdia@gmail.com>
2020-02-04 13:44:20 -0500

    truck test

--- src/TruckTest.java
+++ src/TruckTest.java
@@ -3,12 +3,12 @@ public class TruckTest {
         Truck t1 = new Truck("123ABC");
         Truck t2 = new Truck("456DEF", 45500, 18, 10);
 
-        t1.setMpg(15.0);
-        t1.fill();
-        t1.drive(150);
-        System.out.println(t1);
-        t1.drive(60);
-        System.out.println(t1);
+//        t1.setMpg(15.0);
+//        t1.fill();
+//        t1.drive(150);
+//        System.out.println(t1);
+//        t1.drive(60);
+//        System.out.println(t1);
 
         t2.drive(500);
         t2.fill(30);
